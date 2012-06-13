e119a303af0982b3b91f41abe60442539820ce07
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 16:01:13 +0100

    try to wire in $resource and break everything!

--- web-app/js/scaffolding.js
+++ web-app/js/scaffolding.js
@@ -1,18 +1,29 @@
+angular.module('albumService', ['ngResource']).factory('Album', function($resource) {
+    var baseUrl = $('body').data('base-url').replace(/index$/, '');
+
+    return $resource(baseUrl + ':id', {id: '@id'}, {
+        update: { method: 'POST' }
+    });
+});
+
 angular.module('scaffolding', []).config([
     '$routeProvider',
     function($routeProvider) {
         $routeProvider.
             when('/list', {template: '/grails-ng/list.html', controller: ListCtrl}).
+            when('/create', {template: '/grails-ng/create.html', controller: CreateCtrl}).
             otherwise({redirectTo: '/list'});
     }
 ]);
 
-function ListCtrl($scope) {
-    $scope.baseUrl = $('body').data('base-url').replace(/index$/, '');
+function ListCtrl($scope, Album) {
+    $scope.list = Album.query();
+}
 
-    jQuery.getJSON($scope.baseUrl + 'list', function(data) {
-        $scope.$apply(function() {
-            $scope.list = data;
+function CreateCtrl($scope, $location, Album) {
+    $scope.save = function() {
+        Album.save($scope.album, function(album) {
+            $location.path('/list');
         });
-    });
+    };
 }
