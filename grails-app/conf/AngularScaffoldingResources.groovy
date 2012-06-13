33ffff64e9cc018b0cdb0a6e4a777f72816e4e1e
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 19:44:09 +0100

    upgrade angular to 1.0.0rc12

--- grails-app/conf/AngularScaffoldingResources.groovy
+++ grails-app/conf/AngularScaffoldingResources.groovy
@@ -1,13 +1,18 @@
+import grails.util.GrailsUtil
+
+def fileVersion = GrailsUtil.isDevelopmentEnv() ? '1.0.0rc12' : '1.0.0rc12.min'
+
 modules = {
     angular {
-        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: 'angular-1.0.0rc10.min.js'], nominify: true
+        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: "angular-${fileVersion}.js"], nominify: true
     }
     'angular-resource' {
-        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: 'angular-resource-1.0.0rc10.min.js'], nominify: true
+		dependsOn 'angular'
+        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: "angular-resource-${fileVersion}.js"], nominify: true
     }
 
     'angular-scaffolding' {
-        dependsOn 'jquery', 'angular', 'angular-resource'
+        dependsOn 'jquery', 'angular-resource'
         resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js', file: 'scaffolding.js']
     }
 }
