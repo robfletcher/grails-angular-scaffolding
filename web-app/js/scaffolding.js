4cd2efe12b0fe8de5a9bfeb00a029b0132460e67
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 21:27:15 +0100

    list working again

--- web-app/js/scaffolding.js
+++ web-app/js/scaffolding.js
@@ -1,24 +1,28 @@
 angular.module('albumService', ['ngResource']).factory('Album', function($resource) {
-    var baseUrl = $('body').data('base-url').replace(/index$/, '');
+	var baseUrl = $('body').data('base-url').replace(/index$/, '');
 
-    return $resource(baseUrl + ':id', {id: '@id'}, {
-        update: { method: 'POST' }
-    });
+	return $resource(baseUrl + ':action/:id', {}, {
+		list: {method: 'GET', params: {action: 'list'}, isArray: true},
+		show: {method: 'GET', params: {action: 'show', id: '@id'}},
+		save: {method: 'POST', params: {action: 'save'}},
+		update: {method: 'POST', params: {action: 'update', id: '@id'}},
+		delete: {method: 'POST', params: {action: 'delete', id: '@id'}}
+	});
 });
 
 angular.module('scaffolding', ['albumService']).config([
-    '$routeProvider',
-    function($routeProvider) {
+	'$routeProvider',
+	function($routeProvider) {
 		var baseUrl = $('body').data('base-url').replace(/index$/, '');
 		$routeProvider.
-            when('/list', {templateUrl: '/grails-ng/list.html', controller: ListCtrl}).
+			when('/list', {templateUrl: '/grails-ng/list.html', controller: ListCtrl}).
 //            when('/create', {templateUrl: '/grails-ng/create.html', controller: CreateCtrl}).
-            otherwise({redirectTo: '/list'});
-    }
+			otherwise({redirectTo: '/list'});
+	}
 ]);
 
 function ListCtrl($scope, Album) {
-    $scope.list = Album.query();
+	$scope.list = Album.list();
 }
 
 //ListCtrl.$inject = ['$scope', 'Album'];
