5fbf2a930e8666f99105177d2f6e66a59b6bcae7
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 20:07:10 +0100

    progress of a sort
    wiring is working now but it's fetching the wrong resource - need to redefine paths for all the resource actions or change UrlMappings to use REST

--- web-app/js/scaffolding.js
+++ web-app/js/scaffolding.js
@@ -6,12 +6,13 @@ angular.module('albumService', ['ngResource']).factory('Album', function($resour
     });
 });
 
-angular.module('scaffolding', []).config([
+angular.module('scaffolding', ['albumService']).config([
     '$routeProvider',
     function($routeProvider) {
-        $routeProvider.
-            when('/list', {template: '/grails-ng/list.html', controller: ListCtrl}).
-            when('/create', {template: '/grails-ng/create.html', controller: CreateCtrl}).
+		var baseUrl = $('body').data('base-url').replace(/index$/, '');
+		$routeProvider.
+            when('/list', {templateUrl: '/grails-ng/list.html', controller: ListCtrl}).
+//            when('/create', {templateUrl: '/grails-ng/create.html', controller: CreateCtrl}).
             otherwise({redirectTo: '/list'});
     }
 ]);
@@ -20,10 +21,12 @@ function ListCtrl($scope, Album) {
     $scope.list = Album.query();
 }
 
-function CreateCtrl($scope, $location, Album) {
-    $scope.save = function() {
-        Album.save($scope.album, function(album) {
-            $location.path('/list');
-        });
-    };
-}
+//ListCtrl.$inject = ['$scope', 'Album'];
+
+//function CreateCtrl($scope, $location, Album) {
+//    $scope.save = function() {
+//        Album.save($scope.album, function(album) {
+//            $location.path('/list');
+//        });
+//    };
+//}
