e517155eb571972bfd01fbeb33a5bec1dc482095
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 09:17:52 +0100

    added a scaffolding.js file

--- grails-app/conf/AngularScaffoldingResources.groovy
+++ grails-app/conf/AngularScaffoldingResources.groovy
@@ -5,4 +5,9 @@ modules = {
     'angular-dev' {
         resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: 'angular-1.0.0rc10.js'], nominify: true
     }
+
+    'angular-scaffolding' {
+        dependsOn 'angular'
+        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js', file: 'scaffolding.js']
+    }
 }
