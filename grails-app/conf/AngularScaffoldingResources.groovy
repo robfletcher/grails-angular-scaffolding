36db28876961e4d8ad34663a8755130bdae51799
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 16:00:54 +0100

    added angular-resource script

--- grails-app/conf/AngularScaffoldingResources.groovy
+++ grails-app/conf/AngularScaffoldingResources.groovy
@@ -2,12 +2,12 @@ modules = {
     angular {
         resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: 'angular-1.0.0rc10.min.js'], nominify: true
     }
-    'angular-dev' {
-        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: 'angular-1.0.0rc10.js'], nominify: true
+    'angular-resource' {
+        resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js/angular', file: 'angular-resource-1.0.0rc10.min.js'], nominify: true
     }
 
     'angular-scaffolding' {
-        dependsOn 'jquery', 'angular'
+        dependsOn 'jquery', 'angular', 'angular-resource'
         resource id: 'js', url: [plugin: 'angular-scaffolding', dir: 'js', file: 'scaffolding.js']
     }
 }
