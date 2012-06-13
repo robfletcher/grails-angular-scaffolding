53c8356084c24ed6df37723db852d98f248de785
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-13 08:59:26 +0100

    plugin descriptor

--- AngularScaffoldingGrailsPlugin.groovy
+++ AngularScaffoldingGrailsPlugin.groovy
@@ -1,71 +1,24 @@
 class AngularScaffoldingGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.0 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
+
+    def version = '1.0-SNAPSHOT'
+    def grailsVersion = '2.0 > *'
+    def dependsOn = []
     def pluginExcludes = [
-        "grails-app/views/error.gsp"
+        'grails-app/conf/BootStrap.groovy',
+        'grails-app/views/**/*'
     ]
 
-    // TODO Fill in these fields
-    def title = "Angular Scaffolding Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def title = 'Angular Scaffolding Plugin'
+    def author = 'Rob Fletcher'
+    def authorEmail = 'rob@freeside.co'
     def description = '''\
-Brief summary/description of the plugin.
+A plugin that enables Grails scaffolding to operate as an Angular.js one-page app.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/angular-scaffolding"
+    def documentation = 'http://freeside.co/grails-angular-scaffolding'
+    def license = 'MIT'
+    def organization = [name: 'Freeside Software', url: 'http://freeside.co/']
+    def issueManagement = [system: 'GitHub', url: 'https://github.com/robfletcher/grails-angular-scaffolding/issues']
+    def scm = [url: 'https://github.com/robfletcher/grails-angular-scaffolding']
 
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.grails-plugins.codehaus.org/browse/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
 }
