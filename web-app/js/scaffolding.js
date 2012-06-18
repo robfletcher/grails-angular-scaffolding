f0e78b6fb3515671ddd06d6d28d1f3f96e2b3f5b
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-18 15:49:42 +0100

    indent angular file properly

--- web-app/js/scaffolding.js
+++ web-app/js/scaffolding.js
@@ -1,81 +1,81 @@
 angular.module('grailsService', ['ngResource']).factory('Grails', function($resource) {
-    var baseUrl = $('body').data('base-url').replace(/index$/, '');
+	var baseUrl = $('body').data('base-url').replace(/index$/, '');
 
-    return $resource(baseUrl + ':action/:id', {id: '@id'}, {
-        list: {method: 'GET', params: {action: 'list'}, isArray: true},
-        get: {method: 'GET', params: {action: 'get'}},
-        save: {method: 'POST', params: {action: 'save'}},
-        update: {method: 'POST', params: {action: 'update'}},
-        delete: {method: 'POST', params: {action: 'delete'}}
-    });
+	return $resource(baseUrl + ':action/:id', {id: '@id'}, {
+		list: {method: 'GET', params: {action: 'list'}, isArray: true},
+		get: {method: 'GET', params: {action: 'get'}},
+		save: {method: 'POST', params: {action: 'save'}},
+		update: {method: 'POST', params: {action: 'update'}},
+		delete: {method: 'POST', params: {action: 'delete'}}
+	});
 });
 
 angular.module('scaffolding', ['grailsService']).config([
-    '$routeProvider',
-    function($routeProvider) {
-        $routeProvider.
-            when('/create', {templateUrl: '/create.html', controller: CreateCtrl}).
-            when('/edit/:id', {templateUrl: '/edit.html', controller: EditCtrl}).
-            when('/list', {templateUrl: '/list.html', controller: ListCtrl}).
-            when('/show/:id', {templateUrl: '/show.html', controller: ShowCtrl}).
-            otherwise({redirectTo: '/list'});
-    }
+	'$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/create', {templateUrl: '/create.html', controller: CreateCtrl}).
+			when('/edit/:id', {templateUrl: '/edit.html', controller: EditCtrl}).
+			when('/list', {templateUrl: '/list.html', controller: ListCtrl}).
+			when('/show/:id', {templateUrl: '/show.html', controller: ShowCtrl}).
+			otherwise({redirectTo: '/list'});
+	}
 ]);
 
 function ListCtrl($scope, $location, Grails) {
-    $scope.list = Grails.list();
+	$scope.list = Grails.list();
 
-    $scope.show = function(item) {
-        $location.path('/show/' + item.id);
-    };
+	$scope.show = function(item) {
+		$location.path('/show/' + item.id);
+	};
 }
 
 function ShowCtrl($scope, $routeParams, $location, Grails) {
-    $scope.item = Grails.get({id: $routeParams.id});
+	$scope.item = Grails.get({id: $routeParams.id});
 
-    $scope.delete = function(item) {
+	$scope.delete = function(item) {
 		item.$delete(function(response) {
 			$location.path('/list');
 		});
-    };
+	};
 }
 
 function CreateCtrl($scope, $location, Grails) {
-    $scope.item = new Grails;
-    $scope.save = function(item) {
-        item.$save(function(response) {
-            $location.path('/show/' + response.id);
-        }, function(response) {
-            switch (response.status) {
-                case 422:
-                    $scope.errors = response.data.errors;
-                    break;
-            }
-        });
-    };
+	$scope.item = new Grails;
+	$scope.save = function(item) {
+		item.$save(function(response) {
+			$location.path('/show/' + response.id);
+		}, function(response) {
+			switch (response.status) {
+				case 422:
+					$scope.errors = response.data.errors;
+					break;
+			}
+		});
+	};
 }
 
 function EditCtrl($scope, $routeParams, $location, Grails) {
-    $scope.item = Grails.get({id: $routeParams.id});
+	$scope.item = Grails.get({id: $routeParams.id});
 
-    $scope.update = function(item) {
-        item.$update(function(response) {
-            $location.path('/show/' + response.id);
-        }, function(response) {
-            switch (response.status) {
-                case 409:
-                    // TODO: display optimistic lock error
-                    break;
-                case 422:
-                    $scope.errors = response.data.errors;
-                    break;
-            }
-        });
-    };
+	$scope.update = function(item) {
+		item.$update(function(response) {
+			$location.path('/show/' + response.id);
+		}, function(response) {
+			switch (response.status) {
+				case 409:
+					// TODO: display optimistic lock error
+					break;
+				case 422:
+					$scope.errors = response.data.errors;
+					break;
+			}
+		});
+	};
 
-    $scope.delete = function(item) {
-        item.$delete(function(response) {
+	$scope.delete = function(item) {
+		item.$delete(function(response) {
 			$location.path('/list');
-        });
-    };
+		});
+	};
 }
