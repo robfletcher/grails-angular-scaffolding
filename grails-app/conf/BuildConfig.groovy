abdf8bc6e7eeda39890ec744c8720a860dbe8eb9
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-06-19 18:59:23 +0100

    tidy up plugin declarations

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -16,7 +16,7 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(':release:2.0.3') {
+        build(':release:2.0.3', ':rest-client-builder:1.0.2') {
             export = false
         }
     }
