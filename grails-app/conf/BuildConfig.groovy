e5989e2b63e4bf67d6bee04c6360ec1b138d6544
Rob Fletcher <robert.w.fletcher@gmail.com>
2012-09-28 23:12:23 +0100

    upgrade to grails 2.1.1

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -5,18 +5,21 @@ grails.project.test.reports.dir = 'target/test-reports'
 grails.project.target.level = 1.6
 
 grails.project.dependency.resolution = {
+
     inherits 'global'
     log 'warn'
+
     repositories {
         grailsCentral()
         mavenCentral()
         mavenLocal()
     }
+
     dependencies {
     }
 
     plugins {
-        build(':release:2.0.3', ':rest-client-builder:1.0.2') {
+        build(':release:2.0.4', ':rest-client-builder:1.0.2') {
             export = false
         }
     }
